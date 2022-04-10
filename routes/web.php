478cc43dfcd8853416838fa8c340935cea9d1748
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-04-10 23:30:41 +0700

    Struktur Folder, Route & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('home');
+});
+
+Route::get('/about', function () {
+	return view('about', [
+		'nama' => 'Deo Subarno',
+		'email' => 'aprodeosubarno@gmail.com',
+		'pekerjaan' => 'Software Developer',
+		'image' => 'kon.jpg'
+	]);
+});
+
+Route::get('/blog', function () {
+	return view('posts');
 });
