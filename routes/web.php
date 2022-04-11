b4a3a8344d26fdbd53f01fbc53a8183cd9e76ed4
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-04-11 21:15:03 +0700

    Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -37,13 +37,13 @@ Route::get('/blog', function () {
 			'title' => 'Judul Post Pertama',
 			'slug' => 'judul-post-pertama',
 			'author' => 'Deo Subarno',
-			'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos perspiciatis sunt sequi voluptates minima magni a sapiente consequatur odit odio repudiandae, aspernatur ad nam voluptatum suscipit, earum iste quidem veritatis fugiat eius? Laudantium in voluptatum facilis porro nobis alias optio aperiam itaque aliquam! Minima dignissimos delectus harum voluptatum porro fuga distinctio unde. Ea explicabo facere officiis impedit vel illum adipisci ratione dolorum at nam laudantium ad, facilis eligendi iusto repellendus autem hic esse voluptate culpa aut tempora voluptatum recusandae. Excepturi!'
+			'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos perspiciatis sunt sequi voluptates minima magni a sapiente consequatur odit odio repudiandae, aspernatur ad nam voluptatum suscipit'
 		],
 		[
 			'title' => 'Judul Post Kedua',
 			'slug' => 'judul-post-kedua',
 			'author' => 'Inuyama Aoi',
-			'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam aperiam eius minus, vero velit delectus similique suscipit animi commodi veniam sed consequuntur debitis optio dicta expedita voluptate tempora iure, corporis autem culpa totam. Labore soluta fugiat ducimus! Numquam, odio sapiente maxime exercitationem cupiditate dolorum pariatur modi dolores aliquid asperiores laborum placeat illum consequuntur tempore dicta. Voluptatum ratione molestiae eius rem officiis cupiditate. Nulla impedit explicabo, officiis ex cumque reprehenderit inventore velit labore voluptas sapiente deleniti numquam fugiat ea qui possimus dolores? Nam reiciendis animi suscipit quam rem numquam reprehenderit possimus pariatur facilis hic commodi modi, culpa corrupti similique ad alias provident autem ut sed exercitationem corporis. Tempora est facere officia vel, optio, consequatur iste natus obcaecati dolor consectetur quisquam pariatur?'
+			'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam aperiam eius minus, vero velit delectus similique suscipit animi commodi veniam sed consequuntur debitis optio dicta expedita voluptate tempora iure, corporis autem culpa totam. Labore soluta fugiat ducimus! Numquam, odio sapiente maxime exercitationem cupiditate '
 		],
 	];
 
@@ -60,13 +60,13 @@ Route::get('/blog/{slug}', function ($slug) {
 			'title' => 'Judul Post Pertama',
 			'slug' => 'judul-post-pertama',
 			'author' => 'Deo Subarno',
-			'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos perspiciatis sunt sequi voluptates minima magni a sapiente consequatur odit odio repudiandae, aspernatur ad nam voluptatum suscipit, earum iste quidem veritatis fugiat eius? Laudantium in voluptatum facilis porro nobis alias optio aperiam itaque aliquam! Minima dignissimos delectus harum voluptatum porro fuga distinctio unde. Ea explicabo facere officiis impedit vel illum adipisci ratione dolorum at nam laudantium ad, facilis eligendi iusto repellendus autem hic esse voluptate culpa aut tempora voluptatum recusandae. Excepturi!'
+			'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos perspiciatis sunt sequi voluptates minima magni a sapiente consequatur odit odio repudiandae, aspernatur ad nam voluptatum suscipit'
 		],
 		[
 			'title' => 'Judul Post Kedua',
 			'slug' => 'judul-post-kedua',
 			'author' => 'Inuyama Aoi',
-			'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam aperiam eius minus, vero velit delectus similique suscipit animi commodi veniam sed consequuntur debitis optio dicta expedita voluptate tempora iure, corporis autem culpa totam. Labore soluta fugiat ducimus! Numquam, odio sapiente maxime exercitationem cupiditate dolorum pariatur modi dolores aliquid asperiores laborum placeat illum consequuntur tempore dicta. Voluptatum ratione molestiae eius rem officiis cupiditate. Nulla impedit explicabo, officiis ex cumque reprehenderit inventore velit labore voluptas sapiente deleniti numquam fugiat ea qui possimus dolores? Nam reiciendis animi suscipit quam rem numquam reprehenderit possimus pariatur facilis hic commodi modi, culpa corrupti similique ad alias provident autem ut sed exercitationem corporis. Tempora est facere officia vel, optio, consequatur iste natus obcaecati dolor consectetur quisquam pariatur?'
+			'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam aperiam eius minus, vero velit delectus similique suscipit animi commodi veniam sed consequuntur debitis optio dicta expedita voluptate tempora iure, corporis autem culpa totam. Labore soluta fugiat ducimus! Numquam, odio sapiente maxime exercitationem cupiditate '
 		],
 	];
 
